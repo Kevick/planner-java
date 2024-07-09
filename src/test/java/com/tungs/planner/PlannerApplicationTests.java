99fa8721bcf6ced82fc6c04fc61d563f2efd3c68
Tungs <jairneto180801@gmail.com>
2024-07-08 21:56:03 -0300

    helloworld

---
+++ src/test/java/com/tungs/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tungs.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
